494c148febab406b1ee796f53fcd8096fa39416c
ChefCurrrry <k.kichenassamy@yahoo.com>
2024-12-05 21:50:08 +0100

    Commit de Verif

---
+++ js/Inscriptions.js
@@ -0,0 +1,6 @@
+const menuToggle = document.querySelector('.menu-toggle');
+const containerBouton = document.querySelector('.containerBouton');
+
+menuToggle.addEventListener('click', () => {
+    containerBouton.classList.toggle('show'); // Affiche ou masque le menu
+});
