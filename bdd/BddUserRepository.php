b979d9cd95d57a9c1e232139d2f40449caedf8ea
ChefCurrrry <k.kichenassamy@yahoo.com>
2024-12-25 20:26:26 +0100

    25/12/2024 20:19
    Ajustements

--- bdd/BddUserRepository.php
+++ bdd/BddUserRepository.php
@@ -14,6 +14,12 @@ class BddUserRepository implements IUserRepository{
     public function __construct(Database $bdd){
         $this->pdo = $bdd->connect();
     }
+
+    /**
+     * Sauvegarde le nouvel utilisateur dans la base de données avec une insertion de donnée
+     * @param User $user
+     * @return bool
+     */
     public function saveUser(User $user): bool{
         $request = "INSERT INTO User (email, password, admin) VALUES (:email, :password, :isAdmin)";
         $stmt = $this->pdo->prepare($request);
@@ -24,6 +30,11 @@ class BddUserRepository implements IUserRepository{
 
     }
 
+    /**
+     * Cherche l'utilisateur lors de la connexion
+     * @param string $email
+     * @return User|null
+     */
     public function findUserByEmail(string $email): ?User{
         $request = "SELECT email, password, admin FROM User WHERE email = :email";
         $stmt = $this->pdo->prepare($request);
@@ -37,6 +48,12 @@ class BddUserRepository implements IUserRepository{
 
         return new User($tab['email'], $tab['password'], $tab['admin']);
     }
+
+    /**
+     * Retourne l'id de l'utilisateur spécifié
+     * @param User $user
+     * @return int
+     */
     public function getIdOfUser(User $user): int{
         $request = "SELECT id FROM User WHERE email = :email";
         $stmt = $this->pdo->prepare($request);
