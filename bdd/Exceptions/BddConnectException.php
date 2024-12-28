c87dc828e6a049e0a385532a1eef466153141deb
ChefCurrrry <k.kichenassamy@yahoo.com>
2024-12-28 22:53:36 +0100

    28/12/2024 22:53
    Quelques ajustement back

--- bdd/Exceptions/BddConnectException.php
+++ bdd/Exceptions/BddConnectException.php
@@ -8,14 +8,4 @@ class BddConnectException extends \Exception {
     public function __construct($message = "Erreur de connexion à la base de données", $code = 0, Throwable $previous = null){
         parent::__construct($message, $code, $previous);
     }
-
-    public function redirectToForm($formUrl) : void {
-        // Démarrer une session pour stocker le message d'erreur
-        session_start();
-        $_SESSION['error_message'] = $this->getMessage();
-
-        // Rediriger vers la page du formulaire
-        header("Location: $formUrl");
-        exit;
-    }
 }
